630b5cbcb6579d6d30277904d7f2f5f7c3078bac
Paxic23 <100602652+Paxic23@users.noreply.github.com>
2025-05-25 13:23:30 +0200

    Further fixed setup.bat

--- setup.bat
+++ setup.bat
@@ -2,14 +2,14 @@
 echo Running: python setup.py install
 python setup.py --install
 
-echo Waiting 30 seconds...
-timeout /t 30 /nobreak >nul
+echo Waiting 5 seconds...
+timeout /t 5 /nobreak >nul
 
 echo Running: python setup.py build
 python setup.py --build
 
-echo Waiting 30 seconds...
-timeout /t 30 /nobreak >nul
+echo Waiting 5 seconds...
+timeout /t 5 /nobreak >nul
 
 echo Running: python tmrl/__main__.py --install
 python tmrl/__main__.py --install
